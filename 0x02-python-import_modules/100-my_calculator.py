a11b81ed081e530ce5455c653f8c12c123301235
La-Programmer <justinoghenekomeebedi@gmail.com>
2023-12-05 12:22:53 +0000

    Fix made to program 7

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,11 +4,14 @@ from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
     operators = ['+', '-', '*', '/']
     index = 10
+    a = argv[1]
+    b = argv[3]
+    op = argv[2]
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     for i in range(len(operators)):
-        if argv[2] == operators[i]:
+        if op == operators[i]:
             index = (index - 10) + i
             break
         else:
@@ -17,14 +20,14 @@ if __name__ == "__main__":
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     elif index == 0:
-        print('{} {} {} = {}'.format(argv[1], argv[2], argv[3], add(int(argv[1]), int(argv[3]))))
+        print('{} {} {} = {}'.format(a, op, b, add(int(a), int(b))))
         exit(0)
     elif index == 1:
-        print('{} {} {} = {}'.format(argv[1], argv[2], argv[3], sub(int(argv[1]), int(argv[3]))))
+        print('{} {} {} = {}'.format(a, op, b, sub(int(a), int(b))))
         exit(0)
     elif index == 2:
-        print('{} {} {} = {}'.format(argv[1], argv[2], argv[3], mul(int(argv[1]), int(argv[3]))))
+        print('{} {} {} = {}'.format(a, op, b, mul(int(a), int(b))))
         exit(0)
     else:
-        print('{} {} {} = {}'.format(argv[1], argv[2], argv[3], div(int(argv[1]), int(argv[3]))))
+        print('{} {} {} = {}'.format(a, op, b, div(int(a), int(b))))
         exit(0)
