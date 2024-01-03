8604cb5db1a126029d8f6a6f8a162bce591be51a
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-01-03 13:00:58 +0000

    Addition of program 2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""python3 -c 'print(__import__("1-square").__doc__)'
+"""
+
+
+class Square:
+    """python3 -c 'print(__import__("1-square").Square.__doc__)'
+    """
+    def __init__(self, size):
+        self.__size = size
