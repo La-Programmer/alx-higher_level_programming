4f91dcfffba63128667a2b901033a7568ffa3775
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-01-03 13:42:40 +0000

    Addition of program 3

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""python3 -c 'print(__import__("2-square").__doc__)'
+"""
+
+
+class Square:
+    @staticmethod
+    def isfloat(num):
+        """python3 -c 'print(__import__("Square").isfloat.__doc__)'
+        """
+        try:
+            res = num + 1
+            return True
+        except TypeError:
+            return False
+
+    """python3 -c 'print(__import__("2-square").Square.__doc__)'
+    """
+    def __init__(self, size=0):
+        if self.isfloat(size):
+            if float(size) < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        else:
+            raise TypeError("size must be an integer")
