df30b4f4dda33b0c08465c1be7f3363629af0488
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-01-10 13:47:08 +0000

    Addition of program 5

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""python3 -c 'print(__import__("4-inherits_from.py").__doc__)'
+"""
+
+
+def inherits_from(obj, a_class):
+    """python3 -c 'print(__import__("4-inherits_from.py").inherits_from.__doc__)'
+    """
+    if isinstance(obj, a_class) or issubclass(obj, a_class):
+        return True
+    return False
