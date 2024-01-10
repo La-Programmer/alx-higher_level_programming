1b81f54718f5731135c3cb4a6e467e50172f37c0
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-01-10 13:53:09 +0000

    Addition of program 6

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""python3 -c 'print(__import__("5-base_geometry.py").__doc__)'
+"""
+
+
+class BaseGeometry:
+    """python3 -c 'print(__import__("5-base_geometry").BaseGeometry.__doc__)'
+    """
+    pass
