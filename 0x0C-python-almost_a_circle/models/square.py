094135bfa60e15d830e2f2db8340e82f2876e480
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-01-17 09:22:51 +0100

    Addition of the update method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -40,3 +40,19 @@ class Square(Rectangle):
             raise ValueError("width must be > 0")
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """python3 -c print(__import__("rectangle.py").update.__doc__)
+        """
+        attributes = ["id", "size", "x", "y"]
+        if len(args) > 0:
+            for i in range(min(len(attributes), len(args))):
+                if args[i]:
+                    if attributes[i] == "size":
+                        self.width = args[i]
+                        self.height = args[i]
+                    else:
+                        setattr(self, attributes[i], args[i])
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
