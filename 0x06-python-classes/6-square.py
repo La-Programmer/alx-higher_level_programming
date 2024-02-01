1d6dc3b9b0d5c8fc21b402a518c8387339ca10d5
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-02-01 13:02:27 +0100

    Fix to 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -9,7 +9,7 @@ class Square:
     def __init__(self, size=0, position=(0, 0)):
         self.__size = size
         if isinstance(position, tuple) and len(position) == 2:
-            if position[0] > 0 and position[1] > 0:
+            if position[0] >= 0 and position[1] >= 0:
                 self.__position = position
         else:
             raise TypeError("position must be a tuple of 2 positive integers")
@@ -43,7 +43,8 @@ class Square:
         """python3 -c 'print(__import__("6-square").position.__doc__)'
         """
         if isinstance(value, tuple) and len(value) == 2:
-            self.__position = value
+            if value[0] >= 0 and value[1] >= 0:
+                self.__position = value
         else:
             raise TypeError("position must be a tuple of 2 positive integers")
 
@@ -57,19 +58,19 @@ class Square:
         """
         y = 0
         i = 0
-        while (y < self.__position[1]):
+        while (y < self.position[1]):
             print("")
             y += 1
-        while (i < self.__size):
+        while (i < self.size):
             j = 0
             x = 0
-            while (x < self.__position[0]):
+            while (x < self.position[0]):
                 print(" ", end="")
                 x += 1
-            while (j < self.__size):
+            while (j < self.size):
                 print("#", end="")
                 j += 1
-            print("")
             i += 1
-        if size == 0:
+            print("")
+        if self.size == 0:
             print("")
