f7135d8f8f10ad6c9f8af3fb35827b1e358ee7de
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-03-13 16:52:34 +0100

    Fix made to program 100

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,11 +2,10 @@
 
 const list = require('./100-data').list;
 
-let counter = 0;
 const newList = list.map((item, counter) => {
-	let newItem = item * counter;
-	counter++;
-	return newItem;
+  const newItem = item * counter;
+  counter++;
+  return newItem;
 });
 console.log(list);
 console.log(newList);
