5ce889ba060041c85c92817e397ddf6ee35daa7b
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-03-16 18:12:01 +0100

    Fix made to script 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
-
+"""MODULE 4 FETCHING CITIES WITH RESPECT TO STATES"""
 import MySQLdb
 import sys
 
 
 def run():
-	"""Fetching from database"""
-	username = sys.argv[1]
-	password = sys.argv[2]
-	database = sys.argv[3]
+    """Fetching from database"""
+    u = sys.argv[1]
+    p = sys.argv[2]
+    d = sys.argv[3]
 
-	db = MySQLdb.connect(host='localhost', user=username, passwd=password, db=database, port=3306)
-	cur = db.cursor()
-	sql = "SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id=states.id ORDER BY cities.id ASC"
-	cur.execute(sql)
-	rows = cur.fetchall()
-	for row in rows:
-		print(row)
+    db = MySQLdb.connect(host='localhost', user=u, passwd=p, db=d, port=3306)
+    cur = db.cursor()
+    sql = """
+    SELECT cities.id,
+    cities.name, states.name
+    FROM cities INNER JOIN states ON cities.state_id=states.id
+    ORDER BY cities.id ASC"""
+    cur.execute(sql)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
 
 if __name__ == "__main__":
-	run()
+    run()
