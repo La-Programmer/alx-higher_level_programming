5c5693c4a069492a8ee88d517a41bfa0735c7d7a
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-03-16 16:58:35 +0100

    Fix made to script 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
-
+"""MODULE 2 TO FILTER STATES BY USER INPUT"""
 import MySQLdb
 import sys
 
 
 def run():
-	"""Fetching from database"""
-	username = sys.argv[1]
-	password = sys.argv[2]
-	database = sys.argv[3]
-	search_var = sys.argv[4]
+    """Fetching from database"""
+    u = sys.argv[1]
+    p = sys.argv[2]
+    d = sys.argv[3]
+    search_var = sys.argv[4]
 
-	db = MySQLdb.connect(host='localhost', user=username, passwd=password, db=database, port=3306)
-	cur = db.cursor()
+    db = MySQLdb.connect(host='localhost', user=u, passwd=p, db=d, port=3306)
+    cur = db.cursor()
+    sql = f"""SELECT states.id AS id,
+    states.name AS name
+    FROM states WHERE name='{search_var}' ORDER BY id ASC"""
+    cur.execute(sql)
 
-	cur.execute(f"SELECT states.id AS id, states.name AS name FROM states WHERE name='{search_var}'")
-
-	rows = cur.fetchall()
-	for row in rows:
-		print(row)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
 
 
 if __name__ == "__main__":
-	run()
+    run()
