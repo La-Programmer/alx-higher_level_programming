024b4d4ce5e4aa221b4932c9de3edc9a6cebfa79
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-03-16 16:37:28 +0100

    Fix made to script 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,23 +5,26 @@ import MySQLdb
 
 
 def run():
-	"""Fetching from database"""
-	username = sys.argv[1]
-	password = sys.argv[2]
-	database = sys.argv[3]
+    """Fetching from database"""
+    u = sys.argv[1]
+    p = sys.argv[2]
+    d = sys.argv[3]
 
-	db = MySQLdb.connect(host='localhost', user=username, passwd=password, db=database, port=3306)
-	cur = db.cursor()
+    db = MySQLdb.connect(host='localhost', user=u, passwd=p, db=d, port=3306)
+    cur = db.cursor()
+    sq = """
+    SELECT states.id AS id,
+    states.name AS name
+    FROM states ORDER BY id ASC"""
+    cur.execute(sq)
 
-	cur.execute("SELECT states.id AS id, states.name AS name FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
 
-	rows = cur.fetchall()
-	for row in rows:
-		print(row)
-
-	cur.close()
-	db.close()
+    cur.close()
+    db.close()
 
 
 if __name__ == "__main__":
-	run()
+    run()
