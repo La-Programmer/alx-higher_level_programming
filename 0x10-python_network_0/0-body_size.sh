eb7c17bd922ca47bb5a7c0ab3ab628d3539b1a36
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-04-02 06:31:47 -0700

    FILE: Addition of reademe file and script to get the size of the response gotten from curl command

---
+++ 0x10-python_network_0/0-body_size.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Script to return the size of the response
+if [ $# -lt 1 ]; then
+    echo "IP address / URL required"
+    exit 1
+fi
+curl -s -w "%{size_download}\n" -o /dev/null "$1"
