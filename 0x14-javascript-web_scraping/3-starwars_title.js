bdbb124feb5f2e6348b6a99d89583569704802b8
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-04-23 03:53:35 -0700

    FIX: Bug fix to script 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,9 +4,9 @@ const request = require('request');
 
 const getCmdArgs = () => process.argv.slice(2);
 
-const id = getCmdArgs[0];
+const id = getCmdArgs()[0];
 
-const url = `https://swapi-api.alx-tools.com/api/films/:${id}`;
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
 request(url, (err, res, body) => {
   if (err) {
