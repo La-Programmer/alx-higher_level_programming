0a49a94cf7a157b8c855495f668dd05af8892fb1
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-04-23 16:20:27 +0100

    FILE: Addition of script 101

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const getCmdArgs = () => process.argv.slice(2);
+
+const movieId = getCmdArgs()[0];
+
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const result = JSON.parse(body);
+    console.log(result.characters);
+    for (let i = 0; i < result.characters.length; i++) {
+      const newUrl = result.characters[i];
+      request(newUrl, (err, res, body) => {
+        if (err) {
+          console.log(err);
+        } else {
+          const character = JSON.parse(body);
+          console.log(`Character name: ${character.name}  character ID: ${character.url}`);
+        }
+      });
+    }
+  }
+});
