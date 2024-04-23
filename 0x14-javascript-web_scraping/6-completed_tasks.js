e12dc00e674a1e9a9502ebbfd0cc8c8f136e49b1
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-04-23 12:57:35 +0100

    FILE: Addition of script 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const getCmdArgs = () => process.argv.slice(2);
+
+const url = getCmdArgs()[0];
+
+request(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(body);
+  }
+});
