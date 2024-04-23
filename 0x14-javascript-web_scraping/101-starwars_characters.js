82fef15fb6125507b997bf198691c9d0805f8072
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-04-23 16:43:58 +0100

    FIX: Bug fix to script 101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
-const Promise = require('bluebird');
+// const Promise = require('bluebird');
 
 const getCmdArgs = () => process.argv.slice(2);
 
@@ -15,34 +15,34 @@ request(url, (err, res, body) => {
   } else {
     const result = JSON.parse(body);
     console.log(result.characters);
-    // for (let i = 0; i < result.characters.length; i++) {
-    //   const newUrl = result.characters[i];
-    //   console.log(newUrl);
-    //   request(newUrl, (err, res, body) => {
-    //     if (err) {
-    //       console.log(err);
-    //     } else {
-    //       const character = JSON.parse(body);
-    //       console.log(`Character name: ${character.name}  character ID: ${character.url}`);
-    //     }
-    //   });
-    // }
-    const characterPromises = result.characters.map(newUrl => {
-      return new Promise((resolve, reject) => {
-        request(newUrl, (err, res, body) => {
-          if (err) {
-            reject(err);
-          } else {
-            const character = JSON.parse(body);
-            resolve(`Name: ${character.name}  ID: ${character.url}`);
-          }
-        });
+    for (let i = 0; i < result.characters.length; i++) {
+      const newUrl = result.characters[i];
+      console.log(newUrl);
+      request(newUrl, (err, res, body) => {
+        if (err) {
+          console.log(err);
+        } else {
+          const character = JSON.parse(body);
+          console.log(`Character name: ${character.name}  character ID: ${character.url}`);
+        }
       });
-    });
-    Promise.all(characterPromises)
-      .then(characters => {
-        characters.forEach(character => console.log(character));
-      })
-      .catch(error => console.error(error));
+    }
+    //     const characterPromises = result.characters.map(newUrl => {
+    //       return new Promise((resolve, reject) => {
+    //         request(newUrl, (err, res, body) => {
+    //           if (err) {
+    //             reject(err);
+    //           } else {
+    //             const character = JSON.parse(body);
+    //             resolve(`Name: ${character.name}  ID: ${character.url}`);
+    //           }
+    //         });
+    //       });
+    //     });
+    //     Promise.all(characterPromises)
+    //       .then(characters => {
+    //         characters.forEach(character => console.log(character));
+    //       })
+    //       .catch(error => console.error(error));
   }
 });
