0d644c697ed26ac30fb6526d790b81025b2892f1
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-04-23 12:51:06 +0100

    FILE: Addition of script 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+const getCmdArgs = () => process.argv.slice(2);
+
+const url = getCmdArgs()[0];
+const filePath = getCmdArgs()[1];
+request(url, (err, res, body) => {
+    if (err) {
+        console.log(err);
+    } else {
+        fs.writeFile(filePath, body, 'utf8', (err) => {
+            if (err) {
+                console.log(err);
+            }
+        });
+    }
+})
