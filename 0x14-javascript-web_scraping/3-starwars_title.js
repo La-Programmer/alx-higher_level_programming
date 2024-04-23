a5e4f168e1060498963a329e91181bd05a1674e8
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-04-23 11:27:54 +0100

    FILE: Addition of script 3 which queries the Star Wars API

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const getCmdArgs = () => process.argv.slice(2);
+
+const id = getCmdArgs[0];
+
+const url = `https://swapi-api.alx-tools.com/api/films/:${id}`;
+
+request(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(body);
+  }
+});
