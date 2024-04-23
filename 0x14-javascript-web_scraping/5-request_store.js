9e5c996c26f39a4b55ed733f7c5d4105066ce470
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-04-23 12:52:55 +0100

    FIX: Making script 5 semistandard complaint

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -8,13 +8,13 @@ const getCmdArgs = () => process.argv.slice(2);
 const url = getCmdArgs()[0];
 const filePath = getCmdArgs()[1];
 request(url, (err, res, body) => {
-    if (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(filePath, body, 'utf8', (err) => {
+      if (err) {
         console.log(err);
-    } else {
-        fs.writeFile(filePath, body, 'utf8', (err) => {
-            if (err) {
-                console.log(err);
-            }
-        });
-    }
-})
+      }
+    });
+  }
+});
